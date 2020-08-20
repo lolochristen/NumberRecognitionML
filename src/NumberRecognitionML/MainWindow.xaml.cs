489f494608ac7e28b9451206fe799b79269774c3
lolochristen <laurent@laurent-christen.ch>
2020-08-20 16:03:15 +0200

    Code import

--- src/NumberRecognitionML/MainWindow.xaml.cs
+++ src/NumberRecognitionML/MainWindow.xaml.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,9 +25,346 @@ namespace NumberRecognitionML
     /// </summary>
     public partial class MainWindow : Window
     {
+        private System.Windows.Point? _previousPoint;
+        private NumberRecognition _charRecon;
+        private Timer _timer;
+        private bool _clear;
+
         public MainWindow()
         {
             InitializeComponent();
+            _charRecon = new NumberRecognition();
+
+            _timer = new Timer();
+            _timer.Interval = 600;
+            _timer.AutoReset = false;
+            _timer.Elapsed += _timer_Elapsed;
+
+            Trace.Listeners.Clear();
+            Trace.Listeners.Add(new DelegateTraceListener((message, newline) =>
+            {
+                string s = message + (newline ? "\n" : "");
+                Dispatcher.InvokeAsync(() =>
+                {
+                    Log.Text += s;
+                    Log.ScrollToEnd();
+                });
+            }));
+        }
+
+        private void DrawCanvas_OnStylusDown(object sender, StylusDownEventArgs e)
+        {
+            var p = e.GetPosition(DrawCanvas);
+            if (_clear)
+                DrawCanvas.Children.Clear();
+            _previousPoint = p;
+        }
+
+        private void DrawCanvas_OnStylusMove(object sender, StylusEventArgs e)
+        {
+            if (_previousPoint == null)
+                return;
+
+            var p = e.GetPosition(DrawCanvas);
+            var sp = e.GetStylusPoints(DrawCanvas);
+
+            foreach (var stylusPoint in sp)
+            {
+                p = stylusPoint.ToPoint();
+                var line = new Line()
+                {
+                    StrokeThickness = 40 * stylusPoint.PressureFactor,
+                    Stroke = new SolidColorBrush(Colors.Black),
+                    StrokeStartLineCap = PenLineCap.Round,
+                    StrokeEndLineCap = PenLineCap.Round,
+                    X1 = _previousPoint.Value.X,
+                    Y1 = _previousPoint.Value.Y,
+                    X2 = p.X,
+                    Y2 = p.Y
+                };
+
+                DrawCanvas.Children.Add(line);
+
+                _previousPoint = p;
+            }
+
+            AdjustViewBox();
+
+            _timer.Stop();
+            _timer.Start();
+
+            _clear = false;
+        }
+
+        private void AdjustViewBox()
+        {
+            var actualWidth = DrawCanvas.ActualWidth;
+            var actualHeight = DrawCanvas.ActualHeight;
+
+            var x1 = DrawCanvas.Children.OfType<Line>().Min(l => l.X1 - l.StrokeThickness / 2);
+            var y1 = DrawCanvas.Children.OfType<Line>().Min(l => l.Y1 - l.StrokeThickness / 2);
+            var x2 = DrawCanvas.Children.OfType<Line>().Max(l => l.X2 + l.StrokeThickness / 2);
+            var y2 = DrawCanvas.Children.OfType<Line>().Max(l => l.Y2 + l.StrokeThickness / 2);
+
+
+            // keep aspect ratio and center it
+            var width = x2 - x1;
+            var height = y2 - y1;
+
+            if (width > height)
+            {
+                y1 -= (width - height) / 2;
+                y2 += (width - height) / 2;
+            }
+            else
+            {
+                x1 -= (height - width) / 2;
+                x2 += (height - width) / 2;
+            }
+
+            //var rectangle = DrawCanvas.Children.OfType<System.Windows.Shapes.Rectangle>().FirstOrDefault();
+            //if (rectangle == null)
+            //{
+            //    rectangle = new System.Windows.Shapes.Rectangle() { Stroke = new SolidColorBrush(Colors.Blue), StrokeThickness = 1 };
+            //    DrawCanvas.Children.Add(rectangle);
+            //}
+            //rectangle.Width = x2 - x1;
+            //rectangle.Height = y2 - y1;
+            //rectangle.Margin = new Thickness(x1, y1, 0, 0);
+
+            ResizeVisualBrush.Viewbox = new Rect(new System.Windows.Point(x1 + 2, y1 + 2), new System.Windows.Point(x2 + 2, y2 + 2));
+        }
+        private void DrawCanvas_OnStylusLeave(object sender, StylusEventArgs e)
+        {
+            _previousPoint = null;
+        }
+
+
+        private void DrawCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            _previousPoint = e.GetPosition(DrawCanvas);
+            if (_clear)
+                DrawCanvas.Children.Clear();
+        }
+
+        private void DrawCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_previousPoint != null && e.StylusDevice == null)
+            {
+                var p = e.GetPosition(DrawCanvas);
+
+                var line = new Line()
+                {
+                    StrokeThickness = 20,
+                    Stroke = new SolidColorBrush(Colors.Black),
+                    StrokeStartLineCap = PenLineCap.Round,
+                    StrokeEndLineCap = PenLineCap.Round,
+                    X1 = _previousPoint.Value.X,
+                    Y1 = _previousPoint.Value.Y,
+                    X2 = p.X,
+                    Y2 = p.Y
+                };
+
+                DrawCanvas.Children.Add(line);
+
+                _previousPoint = p;
+
+                AdjustViewBox();
+
+                _timer.Stop();
+                _timer.Start();
+
+                _clear = false;
+            }
+        }
+
+        private void DrawCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            _previousPoint = null;
+            try
+            {
+                PredictDigit();
+            }
+            catch
+            {
+                Trace.WriteLine("Prediction failed. Model loaded?");
+            }
+        }
+
+        private void ClearButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            DrawCanvas.Children.Clear();
+        }
+
+        private BitmapFrame ResizeDrawCanvas()
+        {
+            var visual = AdjustedDrawCanvas;
+
+            var actualWidth = visual.ActualWidth;
+            var actualHeight = visual.ActualHeight;
+
+            RenderTargetBitmap bitmap = new RenderTargetBitmap((int)actualWidth, (int)actualHeight, 96, 96, PixelFormats.Pbgra32);
+            bitmap.Render(visual);
+            var frame = CreateResizedImage(bitmap, 28, 28, 3); // MNIST pictures 28x28 but character is 22x22 with a margin
+            ScaledImage.Source = frame;
+
+            //var encoder = new PngBitmapEncoder();
+            //encoder.Frames.Add(frame);
+            //using (var stream = File.Create($"{Guid.NewGuid()}.png"))
+            //    encoder.Save(stream);
+
+            return frame;
+        }
+
+        private void PredictDigit()
+        {
+            var pixels = GetPixelArray();
+            var digit = new Digit() { PixelValues = pixels };
+
+            var predict = _charRecon.PredictDigit(digit);
+            int i2 = -1;
+            float score = 0;
+            for (int i = 0; i < predict.Score.Length; i++)
+            {
+                if (predict.Score[i] > score)
+                {
+                    i2 = i;
+                    score = predict.Score[i];
+                }
+                Trace.WriteLine($"{i}: {predict.Score[i] * 100:00.0000}%");
+            }
+            Trace.WriteLine($" It's a {i2} with a score of {score * 100:00.0000}%");
+
+            if (score > 0.3)
+            {
+                PredictedNunbers.Text += i2.ToString();
+                PredictedCharacter.Text = i2.ToString();
+            }
+            else
+            {
+                PredictedNunbers.Text += "?";
+                PredictedCharacter.Text = "?";
+            }
+        }
+
+        private float[] GetPixelArray()
+        {
+            var frame = ResizeDrawCanvas();
+
+            int height = frame.PixelHeight;
+            int width = frame.PixelWidth;
+            int nStride = (frame.PixelWidth * frame.Format.BitsPerPixel + 7) / 8;
+            byte[] pixelByteArray = new byte[frame.PixelHeight * nStride];
+            frame.CopyPixels(pixelByteArray, nStride, 0);
+
+            float[] pixelFix = new float[height * width];
+            for (int i = 0; i < pixelByteArray.Length; i += 4)
+            {
+                var i2 = i / 4;
+                pixelFix[i2] = pixelByteArray[i + 1];
+                if (pixelFix[i2] < pixelByteArray[i + 2])
+                    pixelFix[i2] = pixelByteArray[i + 2];
+                if (pixelFix[i2] < pixelByteArray[i + 3])
+                    pixelFix[i2] = pixelByteArray[i + 3];
+            }
+
+            return pixelFix;
+        }
+
+        private static BitmapFrame CreateResizedImage(ImageSource source, int width, int height, int margin)
+        {
+            var rect = new Rect(margin, margin, width - margin * 2, height - margin * 2);
+
+            var group = new DrawingGroup();
+            RenderOptions.SetBitmapScalingMode(group, BitmapScalingMode.HighQuality);
+            group.Children.Add(new ImageDrawing(source, rect));
+
+            var drawingVisual = new DrawingVisual();
+            using (var drawingContext = drawingVisual.RenderOpen())
+                drawingContext.DrawDrawing(group);
+
+            var resizedImage = new RenderTargetBitmap(
+                width, height,         // Resized dimensions
+                96, 96,                // Default DPI values
+                PixelFormats.Default); // Default pixel format
+            resizedImage.Render(drawingVisual);
+
+            return BitmapFrame.Create(resizedImage);
+        }
+
+        private void TrainButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog() { Filter = "MNIST Data Files|*.csv", DefaultExt = ".csv" };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog() { Filter = "Model|*.zip|All|*.*", DefaultExt = ".zip", FileName = "NumberRecognitionModel.zip" };
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    Task.Run(() => _charRecon.Train(openFileDialog.FileName, saveFileDialog.FileName));
+                }
+            }
+        }
+
+        private void LoadModelButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog() { Filter = "Model|*.zip|All|*.*", DefaultExt = ".zip", FileName = "NumberRecognitionModel.zip" };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _charRecon.LoadModel(openFileDialog.FileName);
+            }
+        }
+
+        private void AppendData_Click(object sender, RoutedEventArgs e)
+        {
+            var pixels = GetPixelArray();
+            var n = int.Parse(Character.Text);
+            using (var fs = File.OpenWrite("training_data.csv"))
+            {
+                fs.Seek(0, SeekOrigin.End);
+                using (var writer = new StreamWriter(fs))
+                {
+                    writer.Write(n);
+                    foreach (var f in pixels)
+                    {
+                        writer.Write(',');
+                        writer.Write(f.ToString());
+                    }
+                    writer.WriteLine();
+                }
+            }
+            Trace.WriteLine($" {n} added to trainging_data.csv");
+            ClearButton_OnClick(sender, e);
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            _timer.Stop();
+            _clear = true;
+            Dispatcher.Invoke(() =>
+            {
+                if (PredictsButton.IsChecked == true)
+                    PredictDigit();
+            });
+        }
+    }
+
+    public class DelegateTraceListener : TraceListener
+    {
+        private readonly Action<string, bool> _func;
+
+        public DelegateTraceListener(Action<string, bool> func)
+        {
+            _func = func;
+        }
+
+        public override void Write(string message)
+        {
+            _func.Invoke(message, false);
+        }
+
+        public override void WriteLine(string message)
+        {
+            _func.Invoke(message, true);
         }
     }
 }
---
+++ src/NumberRecognitionML/NumberRecognition.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberRecognitionML
+{
+    /// <summary>
+    /// The Digit class represents one mnist digit.
+    /// </summary>
+    public class Digit
+    {
+        public float Number;
+        [VectorType(784)] public float[] PixelValues;
+    }
+
+    /// <summary>
+    /// The DigitPrediction class represents one digit prediction.
+    /// </summary>
+    public class DigitPrediction
+    {
+        public float[] Score;
+    }
+
+    public class NumberRecognition
+    {
+        private static bool HasHeaders = true;
+        public void Train(string dataPath, string modelPath)
+        {
+            // create a machine learning context
+            var context = new MLContext();
+
+            // load data
+            Trace.WriteLine("Loading data....");
+            var dataView = context.Data.LoadFromTextFile(
+                path: dataPath,
+                columns: new[]
+                {
+                    new TextLoader.Column("Number", DataKind.Single, 0),
+                    new TextLoader.Column(nameof(Digit.PixelValues), DataKind.Single, 1, 784)
+                },
+                hasHeader: HasHeaders,
+                separatorChar: ',');
+
+            // split data into a training and test set
+            var partitions = context.Data.TrainTestSplit(dataView, testFraction: 0.2);
+
+            // build a training pipeline
+            // step 1: concatenate all feature columns
+            var pipeline = context.Transforms.Concatenate("Features",
+                //DefaultColumnNames.Features,
+                nameof(Digit.PixelValues))
+
+                .Append(context.Transforms.Conversion.MapValueToKey(inputColumnName: "Number", outputColumnName: "Label"))
+
+                // step 2: cache data to speed up training                
+                .AppendCacheCheckpoint(context)
+
+                // step 3: train the model with SDCA
+                .Append(context.MulticlassClassification.Trainers.SdcaMaximumEntropy(
+                    labelColumnName: "Label",
+                    featureColumnName: "Features"));
+
+            // train the model
+            Trace.WriteLine("Training model....");
+            var model = pipeline.Fit(partitions.TrainSet);
+
+
+            // use the model to make predictions on the test data
+            Trace.WriteLine("Evaluating model....");
+            var predictions = model.Transform(partitions.TestSet);
+
+            // evaluate the predictions
+            var metrics = context.MulticlassClassification.Evaluate(
+                data: predictions
+                //label: "Number",
+                //score: "Score" /*DefaultColumnNames.Score*/
+                );
+
+            // show evaluation metrics
+            Trace.WriteLine($"Evaluation metrics");
+            Trace.WriteLine($"    MicroAccuracy:    {metrics.MicroAccuracy:0.###}");
+            Trace.WriteLine($"    MacroAccuracy:    {metrics.MacroAccuracy:0.###}");
+            Trace.WriteLine($"    LogLoss:          {metrics.LogLoss:#.###}");
+            Trace.WriteLine($"    LogLossReduction: {metrics.LogLossReduction:#.###}");
+
+            context.Model.Save(model, dataView.Schema, modelPath);
+            Trace.WriteLine($"Model {modelPath} saved.");
+
+            _predictionEngine = context.Model.CreatePredictionEngine<Digit, DigitPrediction>(model);
+
+            //// grab three digits from the data: 2, 7, and 9
+            //var digits = context.Data.CreateEnumerable<Digit>(dataView, reuseRowObject: false).ToArray();
+            //var testDigits = new Digit[] { digits[5], digits[12], digits[20] };
+
+            //// create a prediction engine
+            //var engine = context.Model.CreatePredictionEngine<Digit, DigitPrediction>(model);
+            ////var engine = model.CreatePredictionEngine<Digit, DigitPrediction>(context);
+
+            //// predict each test digit
+            //for (var i = 0; i < testDigits.Length; i++)
+            //{
+            //    var prediction = engine.Predict(testDigits[i]);
+
+            //    // show results
+            //    Console.WriteLine($"Predicting test digit {i}...");
+            //    for (var j = 0; j < 10; j++)
+            //    {
+            //        Console.WriteLine($"  {j}: {prediction.Score[j]:P2}");
+            //    }
+            //    Console.WriteLine();
+            //}
+        }
+
+        private PredictionEngine<Digit, DigitPrediction> _predictionEngine;
+
+        public void LoadModel(string modelPath)
+        {
+            var context = new MLContext();
+            DataViewSchema schema;
+            var model = context.Model.Load(modelPath, out schema);
+            _predictionEngine = context.Model.CreatePredictionEngine<Digit, DigitPrediction>(model);
+            Trace.WriteLine($"Model {modelPath} loaded.");
+        }
+
+        public DigitPrediction PredictDigit(Digit digit)
+        {
+            try
+            {
+                return _predictionEngine.Predict(digit);
+            }
+            catch(Exception exp)
+            {
+                Trace.WriteLine("Prediction failed. Model loaded?");
+                return null;
+            }
+        }
+
+        public char PredictDigitEvaluate(Digit digit, out float score)
+        {
+            var digitPred = _predictionEngine.Predict(digit);
+            score = digitPred.Score.Max();
+            for (int i = 0; i < digitPred.Score.Length; i++)
+                if (digitPred.Score[i] == score)
+                    return (char) (i + 48);
+            return ' ';
+        }
+    }
+}
